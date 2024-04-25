8290c247a1c6e44d6b836751ebe74e1065f663aa
Sachin Jaiswal <sachin.jaiswal@techdome.net.in>
2024-04-25 12:52:45 +0530

    info changed

--- program1.py
+++ program1.py
@@ -14,6 +14,6 @@ class Solution(object):
                 if not stack or stack.pop() != brackets[char]:
                     return False
             else:
-                return False
+                return False 
 
         return not stack
